faaebb2d340c8267ec9e3dc5d766dcdbfe23b09a
Markus Kreth <markus.kreth@web.de>
2018-02-22 19:12:22 +0100

    more tests

--- src/test/java/de/kreth/kata/stringsum/StringCalculatorTest.java
+++ src/test/java/de/kreth/kata/stringsum/StringCalculatorTest.java
@@ -26,4 +26,25 @@ class StringCalculatorTest {
 		assertEquals(5, StringCalculator.sum("2,3"));
 		assertEquals(0, StringCalculator.sum("0,0"));
 	}
+
+	@Test
+	void differentStringsProduce0() {
+		assertInputProducesResult(0, "", "0,0", "0,0,0,0", "-1,1", "-1,1,-1,1");
+	}
+
+	@Test
+	void differentStringsProduce1() {
+		assertInputProducesResult(1, "1", "1,0", "0,1", "0,0,1,0", "-1,2", "-1,1,1");
+	}
+
+	@Test
+	void bigValuesSumm() {
+		assertInputProducesResult(100, "100,0", "50,50", "25,25,25,25", "1000,-900");
+	}
+	
+	void assertInputProducesResult(int result, String... inputs) {
+		for (String input: inputs) {
+			assertEquals(result, StringCalculator.sum(input));			
+		}
+	}
 }
